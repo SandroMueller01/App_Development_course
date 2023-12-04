5f18cd35c2262dedb141bfbf8dbf40a1c915ebf6
SandroMueller01 <93937204+SandroMueller01@users.noreply.github.com>
2023-12-04 17:00:44 +0100

    login_begin
    begin of login has to be changed to linear layout

--- App/app/build.gradle.kts
+++ App/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.griptrainerapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.griptrainerapp"
